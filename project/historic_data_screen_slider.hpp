4d175a42d23e2defae43a7a6cb5fc74dbc461212
DJKhalil <khalil.salame2004@gmail.com>
2025-11-19 14:41:48 +0100

    Update historic_data_screen_slider.hpp
    Jag försöker fixa en slider till historic data screen

--- project/historic_data_screen_slider.hpp
+++ project/historic_data_screen_slider.hpp
@@ -1,1 +1,20 @@
 
+#ifndef HISTORIC_DATA_SCREEN_SLIDER
+#define HISTORIC_DATA_SCREEN_SLIDER
+
+#include <Arduino.h>
+#include <LV_Helper.h>
+#include <LilyGo_AMOLED.h>
+#include <TFT_eSPI.h>
+#include <lvgl.h>
+
+lv_obj_t *slider = lv_slider_create(lv_scr_act()):
+lv_obj_align(slider, LV_ALIGN_CENTER, 0,-10);
+
+lv_slider_set_range(slider, 0, 11);
+
+static void slider_event_cb(lv_event_t * e){
+  lv_obj_t * slider = lv_event_get_target(e);
+  int value = lv_slider_get_value(slider);
+}
+
